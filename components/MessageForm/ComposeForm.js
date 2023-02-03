cf95835927b03e78bba328b2893c3c319859bb40
Aman-codde <amandeepk221@gmail.com>
2023-02-03 15:08:42 -0800

    feat(components): handle user input in generic way

--- components/MessageForm/ComposeForm.js
+++ components/MessageForm/ComposeForm.js
@@ -1,23 +1,37 @@
 import Input from "./Input";
 import { View } from "react-native";
-
-function composeHandler() {}
+import { useState } from "react";
 
 function ComposeForm() {
+  const [inputValues, setInputValues] = useState({
+    receiver: "",
+    sender: "aman",
+    title: "",
+    body: "",
+  });
+
+  function inputChangeHandler(inputIdentifier, enteredInput) {
+    setInputValues((curInputValues) => {
+      return { ...curInputValues, [inputIdentifier]: enteredInput };
+    });
+  }
+
   return (
     <View>
       <Input
         placeholder="Recipient"
         textInputConfig={{
           maxLength: 10,
-          onChangeText: composeHandler,
+          onChangeText: inputChangeHandler.bind(this, "receiver"),
+          value: inputValues.receiver,
         }}
       ></Input>
       <Input
         placeholder="Subject"
         textInputConfig={{
           maxLength: 50,
-          onChangeText: composeHandler,
+          onChangeText: inputChangeHandler.bind(this, "title"),
+          value: inputValues.title,
         }}
       ></Input>
       <Input
@@ -25,8 +39,9 @@ function ComposeForm() {
         textInputConfig={{
           multiline: true,
           maxLength: 100,
-          onChangeText: composeHandler,
+          onChangeText: inputChangeHandler.bind(this, "body"),
           autocorrect: false,
+          value: inputValues.body,
         }}
       ></Input>
     </View>
