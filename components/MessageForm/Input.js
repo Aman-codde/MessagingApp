9d24ff034fd93d4337a99b37867f82a8c7499d8d
Aman-codde <amandeepk221@gmail.com>
2023-02-03 14:34:28 -0800

    styles(MessageForm): add styles for Input component

--- components/MessageForm/Input.js
+++ components/MessageForm/Input.js
@@ -1,12 +1,42 @@
-import { Text, TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 
-function Input({ label, textInputConfig }) {
+function Input({ textInputConfig, placeholder }) {
+  const inputStyles = [styles.input];
+  if (textInputConfig && textInputConfig.multiline) {
+    inputStyles.push(styles.inputMultiline);
+  }
+
   return (
-    <View>
-      <Text>{label}</Text>
-      <TextInput {...textInputConfig}></TextInput>
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={inputStyles}
+        {...textInputConfig}
+        placeholder={placeholder}
+      ></TextInput>
     </View>
   );
 }
 
 export default Input;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    alignContent: "left",
+    marginHorizontal: 4,
+    marginVertical: 16,
+  },
+  label: {
+    fontSize: 16,
+    marinBottom: 4,
+  },
+  input: {
+    padding: 10,
+    fontSize: 18,
+    borderWidth: 1,
+    borderRadius: 4,
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: "top",
+  },
+});
