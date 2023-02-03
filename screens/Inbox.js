f4ab490101cfba2d59bdc37e21ebb3c4ad512b89
Aman-codde <amandeepk221@gmail.com>
2023-02-03 04:24:35 -0800

    feat(screens): use Context to display messages

--- screens/Inbox.js
+++ screens/Inbox.js
@@ -1,7 +1,15 @@
+import { useContext } from "react";
 import InboxOutput from "../components/MessagesOutput/InboxOutput";
+import { MessagesContext } from "../store/messages-context";
 
 function Inbox() {
-  return <InboxOutput />;
+  const messagesCtx = useContext(MessagesContext);
+
+  const inboxMessages = messagesCtx.messages.filter(
+    (message) => message.receiver == "aman"
+  );
+
+  return <InboxOutput inboxMessages={inboxMessages} />;
 }
 
 export default Inbox;
--- screens/SentMessages.js
+++ screens/SentMessages.js
@@ -1,7 +1,14 @@
+import { useContext } from "react";
 import SentOutput from "../components/MessagesOutput/SentOutput";
+import { MessagesContext } from "../store/messages-context";
 
 function SentMessages() {
-  return <SentOutput />;
+  const messagesCtx = useContext(MessagesContext);
+  const sentMessages = messagesCtx.messages.filter(
+    (message) => message.sender == "aman"
+  );
+
+  return <SentOutput sentMessages={sentMessages} />;
 }
 
 export default SentMessages;
