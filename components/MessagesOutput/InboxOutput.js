e7fd45aec04bbcf31b51156b863a13380a750fdb
Aman-codde <amandeepk221@gmail.com>
2023-02-03 04:22:28 -0800

    feat(MessagesOutput): remove unused data, pass messages as argument

--- components/MessagesOutput/InboxOutput.js
+++ components/MessagesOutput/InboxOutput.js
@@ -1,34 +1,10 @@
 import { View } from "react-native";
 import InboxList from "./InboxList";
 
-const DUMMY_INBOX_MESSAGES = [
-  {
-    id: "m1",
-    sender: "sender1",
-    receiver: "aman",
-    title: "greeting",
-    body: "Good Morning",
-  },
-  {
-    id: "m2",
-    sender: "sender2",
-    receiver: "aman",
-    title: "invitation",
-    body: "Join the Birthday celebration on Feb 4,2023 at 2:00PM",
-  },
-  {
-    id: "m3",
-    sender: "sender3",
-    receiver: "aman",
-    title: "meeting info",
-    body: "the company meeting is happening on Feb 17,2023 at 3:00PM ",
-  },
-];
-
-function InboxOutput() {
+function InboxOutput({ inboxMessages }) {
   return (
     <View>
-      <InboxList inboxMessages={DUMMY_INBOX_MESSAGES}></InboxList>
+      <InboxList inboxMessages={inboxMessages}></InboxList>
     </View>
   );
 }
--- components/MessagesOutput/SentOutput.js
+++ components/MessagesOutput/SentOutput.js
@@ -1,34 +1,10 @@
 import { View } from "react-native";
 import SentList from "./SentList";
 
-const DUMMY_SENT_MESSAGES = [
-  {
-    id: "s1",
-    sender: "aman",
-    receiver: "receiver1",
-    title: "reply to greeting",
-    body: "Good Morning!! Have a great day!",
-  },
-  {
-    id: "s2",
-    sender: "aman",
-    receiver: "receiver2",
-    title: "reply to birthday invite",
-    body: "Thank you for inviting. I would love to attend!",
-  },
-  {
-    id: "s3",
-    sender: "aman",
-    receiver: "receiver3",
-    title: "reply to meeting info ",
-    body: "I am looking forward to attend the meeting.",
-  },
-];
-
-function SentOutput() {
+function SentOutput({ sentMessages }) {
   return (
     <View>
-      <SentList sentMessages={DUMMY_SENT_MESSAGES}></SentList>
+      <SentList sentMessages={sentMessages}></SentList>
     </View>
   );
 }
