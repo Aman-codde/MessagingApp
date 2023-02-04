94dfd8a3413b433b055f7a6fd73efc3b49adcbf1
Aman-codde <amandeepk221@gmail.com>
2023-02-04 08:35:13 -0800

    feat(http): create get request to get all expenses

--- util/http.js
+++ util/http.js
@@ -2,9 +2,27 @@ import axios from "axios";
 
 //const API_URL = "http://interview.bixly.com/api-token-auth";
 
-export default function storeMessage(messageData) {
-  axios.post(
-    "https://react-native-message-app-default-rtdb.firebaseio.com/messages.json",
-    messageData
-  );
+const BACKEND_URL =
+  "https://react-native-message-app-default-rtdb.firebaseio.com/messages.json";
+
+export function storeMessage(messageData) {
+  axios.post(BACKEND_URL, messageData);
+}
+
+export async function fetchMessages() {
+  const response = await axios.get(BACKEND_URL);
+  console.log("fetched data: ", response.data);
+
+  const messages = [];
+
+  for (let key in response) {
+    const messageObj = {
+      sender: response.data[key].sender,
+      receiver: response.data[key].receiver,
+      title: response.data[key].title,
+      body: response.data[key].body,
+    };
+  }
+
+  return messages;
 }
