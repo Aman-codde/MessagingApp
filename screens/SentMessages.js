f105c798eb73c3c4a3e702ab8dbb728262cf4857
Aman-codde <amandeepk221@gmail.com>
2023-02-05 05:14:45 -0800

    feat(screens): add useEffect to get latest re render updated messages list

--- screens/SentMessages.js
+++ screens/SentMessages.js
@@ -1,12 +1,23 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import MessagesOutput from "../components/MessagesOutput/MessagesOutput";
 import { MessagesContext } from "../store/messages-context";
+import { fetchMessages } from "../util/http";
 
 function SentMessages() {
   const messagesCtx = useContext(MessagesContext);
+
   const sentMessages = messagesCtx.messages.filter(
     (message) => message.sender == "Aman"
   );
+
+  useEffect(() => {
+    async function getMessages() {
+      const messages = await fetchMessages();
+      messagesCtx.setMessages(messages);
+    }
+
+    getMessages();
+  }, []);
 
   const noSentMsgsInfo = "There are no sent messages.";
 
