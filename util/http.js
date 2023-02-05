43e330a380859107733ead73377fb24603fe0a27
Aman-codde <amandeepk221@gmail.com>
2023-02-05 05:08:03 -0800

    fix(http): push object to array, add id, url constant

--- util/http.js
+++ util/http.js
@@ -3,25 +3,25 @@ import axios from "axios";
 //const API_URL = "http://interview.bixly.com/api-token-auth";
 
 const BACKEND_URL =
-  "https://react-native-message-app-default-rtdb.firebaseio.com/messages.json";
+  "https://react-native-message-app-default-rtdb.firebaseio.com";
 
 export function storeMessage(messageData) {
-  axios.post(BACKEND_URL, messageData);
+  axios.post(BACKEND_URL + "/messages.json", messageData);
 }
 
 export async function fetchMessages() {
-  const response = await axios.get(BACKEND_URL);
-  console.log("fetched data: ", response.data);
-
+  const response = await axios.get(BACKEND_URL + "/messages.json");
   const messages = [];
 
-  for (let key in response) {
+  for (let key in response.data) {
     const messageObj = {
+      id: key,
       sender: response.data[key].sender,
       receiver: response.data[key].receiver,
       title: response.data[key].title,
       body: response.data[key].body,
     };
+    messages.push(messageObj);
   }
 
   return messages;
