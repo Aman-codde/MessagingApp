627587ad4c742beb1d902317ed269d931a8bfbb6
Aman-codde <amandeepk221@gmail.com>
2023-02-05 06:25:50 -0800

    feat(screens): create useState isFetching to manage loading state

--- screens/SentMessages.js
+++ screens/SentMessages.js
@@ -1,9 +1,12 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import MessagesOutput from "../components/MessagesOutput/MessagesOutput";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
 import { MessagesContext } from "../store/messages-context";
 import { fetchMessages } from "../util/http";
 
 function SentMessages() {
+  const [isFetching, setIsFetching] = useState(true);
+
   const messagesCtx = useContext(MessagesContext);
 
   const sentMessages = messagesCtx.messages.filter(
@@ -12,12 +15,18 @@ function SentMessages() {
 
   useEffect(() => {
     async function getMessages() {
+      setIsFetching(true);
       const messages = await fetchMessages();
+      setIsFetching(false);
       messagesCtx.setMessages(messages);
     }
 
     getMessages();
   }, []);
+
+  if (isFetching) {
+    return <LoadingOverlay />;
+  }
 
   const noSentMsgsInfo = "There are no sent messages.";
 
