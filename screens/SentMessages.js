83089ab11e71cd87a86f02cd334e13f67f1f6849
Aman-codde <amandeepk221@gmail.com>
2023-02-05 21:50:42 -0800

    feat(ManageMessage): pass auth token

--- screens/SentMessages.js
+++ screens/SentMessages.js
@@ -2,12 +2,14 @@ import { useContext, useEffect, useState } from "react";
 import MessagesOutput from "../components/MessagesOutput/MessagesOutput";
 import ErrorOverlay from "../components/UI/ErrorOverlay";
 import LoadingOverlay from "../components/UI/LoadingOverlay";
+import { AuthContext } from "../store/auth-context";
 import { MessagesContext } from "../store/messages-context";
 import { fetchMessages } from "../util/http";
 
 function SentMessages() {
   const [isFetching, setIsFetching] = useState(true);
   const [error, setError] = useState();
+  const authCtx = useContext(AuthContext);
 
   const messagesCtx = useContext(MessagesContext);
 
@@ -19,7 +21,7 @@ function SentMessages() {
     async function getMessages() {
       setIsFetching(true);
       try {
-        const messages = await fetchMessages();
+        const messages = await fetchMessages(authCtx.token);
         messagesCtx.setMessages(messages);
       } catch (err) {
         setError("Could not fetch messages");
