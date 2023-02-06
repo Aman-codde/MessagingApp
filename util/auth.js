b126bdc0aa4fc76e2fcf82c3b7732775c1551128
Aman-codde <amandeepk221@gmail.com>
2023-02-05 18:35:44 -0800

    feat(auth): return token from api and send to login, createUser

--- util/auth.js
+++ util/auth.js
@@ -9,16 +9,17 @@ async function authenticate(mode, email, password) {
     password: password,
     returnSecureToken: true,
   });
-  console.log(response.data);
+  const token = response.data.idToken;
+  return token;
 }
 
 // email: "aman@aman.com"
 // password: "123456"
 
-export async function createUser(email, password) {
-  await authenticate("signUp", email, password);
+export function createUser(email, password) {
+  return authenticate("signUp", email, password);
 }
 
-export async function login(email, password) {
-  await authenticate("signInWithPassword", email, password);
+export function login(email, password) {
+  return authenticate("signInWithPassword", email, password);
 }
