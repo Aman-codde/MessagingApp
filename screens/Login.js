7632d22deb1cfb8d25590921c475169393526e13
Aman-codde <amandeepk221@gmail.com>
2023-02-05 16:43:37 -0800

    feat(Login): apply login functionality and loading indicator

--- screens/Login.js
+++ screens/Login.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import AuthForm from "../components/Auth/AuthForm";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
+import { login } from "../util/http";
 
 function Login() {
-  return <AuthForm />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function loginHandler({ email, password }) {
+    setIsAuthenticating(true);
+    await login(email, password);
+    setIsAuthenticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Logging in..." />;
+  }
+
+  return <AuthForm onAuthenticate={loginHandler} />;
 }
 
 export default Login;
