0a24c2565a4c91d1456a3dbf530337708ce87b3a
Aman-codde <amandeepk221@gmail.com>
2023-02-07 05:32:32 -0800

    fix(Inbox): add components for error,auth,loading,context and update inbox using context by calling backend api

--- screens/Inbox.js
+++ screens/Inbox.js
@@ -1,17 +1,54 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import MessagesOutput from "../components/MessagesOutput/MessagesOutput";
+import ErrorOverlay from "../components/UI/ErrorOverlay";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
+import { AuthContext } from "../store/auth-context";
 import { MessagesContext } from "../store/messages-context";
+import { fetchInboxMessages } from "../util/http";
 
 function Inbox() {
-  const messagesCtx = useContext(MessagesContext);
+  const [isFetching, setIsFetching] = useState(true);
+  const [error, setError] = useState();
+  const authCtx = useContext(AuthContext);
 
-  const inboxMessages = messagesCtx.messages.filter(
-    (message) => message.receiver == "test"
+  const [context, setContext] = useContext(MessagesContext);
+
+  useEffect(() => {
+    async function getInboxMessages() {
+      setIsFetching(true);
+      try {
+        const inbox_messages = await fetchInboxMessages(authCtx.token);
+        setContext({
+          ...context,
+          inboxMessages: inbox_messages,
+          boxType: "inbox",
+        });
+      } catch (err) {
+        setError("Could not fetch messages");
+      }
+      setIsFetching(false);
+    }
+
+    getInboxMessages();
+  }, []);
+
+  function errorHandler() {
+    setError(null);
+  }
+
+  if (error && !isFetching) {
+    return <ErrorOverlay message={error} onConfirm={errorHandler} />;
+  }
+
+  if (isFetching) {
+    return <LoadingOverlay />;
+  }
+
+  const noInboxMsgsInfo = "The inbox is empty";
+
+  return (
+    <MessagesOutput messages={context.inboxMessages} info={noInboxMsgsInfo} />
   );
-
-  const emptyInboxInfo = "The Inbox is empty";
-
-  return <MessagesOutput messages={inboxMessages} info={emptyInboxInfo} />;
 }
 
 export default Inbox;
