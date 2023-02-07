e18fc8da5f3537339b1368ea833af50c2286ec01
Aman-codde <amandeepk221@gmail.com>
2023-02-07 04:42:02 -0800

    ref(auth): add url constant,reuse it,object destructure

--- util/auth.js
+++ util/auth.js
@@ -1,15 +1,12 @@
 import axios from "axios";
 
-// api-token-auth
-// API URL: http://interview.bixly.com Auth Scheme: Token
+const BACKEND_URL = `http://interview.bixly.com/`;
 
 export async function login(username, password) {
-  const url = `http://interview.bixly.com/api-token-auth/`;
-  const response = await axios.post(url, {
-    username: username,
-    password: password,
+  const response = await axios.post(BACKEND_URL + "api-token-auth/", {
+    username,
+    password,
   });
-  // const userInfo = response.config["data"];
   const token = response.data.token;
   return token;
 }
